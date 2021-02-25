2e0adb5e1a9b9bf8139b8a2d34885f3c1d6532a5
eren dhoheiri <dhoheiri@gmail.com>
2021-02-25 15:22:26 +0700

    mockup layout tips riding

---
+++ src/components/molecules/TipsRidingItem/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {Buttons} from '../../atoms';
+
+const TipsRidingItem = ({navigation}) => {
+  const handleGoTo = (screen) => {
+    navigation.navigate(screen);
+  };
+  return (
+    <View
+      style={{
+        height: 180,
+        borderRadius: 4,
+        backgroundColor: '#ffffff',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: 'rgba(255, 255, 255, 0.08)',
+        flexDirection: 'row',
+        width: '95%',
+        marginHorizontal: 10,
+        marginVertical: 5,
+        borderRadius: 8,
+      }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          padding: 10,
+        }}>
+        <View
+          style={{
+            width: 148,
+            height: 148,
+            backgroundColor: '#eeee',
+            marginRight: 10,
+            borderRadius: 8,
+          }}></View>
+        <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            padding: 10,
+          }}>
+          <Text
+            style={{
+              width: '50%',
+              height: 51,
+              fontFamily: 'Sarabun',
+              fontSize: 13.4,
+              fontWeight: 'normal',
+              fontStyle: 'normal',
+              letterSpacing: 0.5,
+              lineHeight: 20,
+              textAlign: 'justify',
+              color: '#454749',
+            }}>
+            6 Safety points, for children while out riding their bikes.
+          </Text>
+          <View style={{width: '50%'}}>
+            <Buttons
+              backgroundcolor="#0c8eff"
+              text="View Details"
+              textcolor="white"
+              //   onPress={() => handleGoTo('TipsPDF')}
+            />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default TipsRidingItem;
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,5 +1,6 @@
 import ButtonGoogle from './ButtonGoogle';
 import Loading from './Loading';
 import BottomNavigator from './BottomNavigator';
+import TipsRidingItem from './TipsRidingItem';
 
-export {Loading, ButtonGoogle, BottomNavigator};
+export {Loading, ButtonGoogle, BottomNavigator, TipsRidingItem};
